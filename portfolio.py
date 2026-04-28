76416bbbea13316137d93ae31d1bf872e22b6fa7
only1blino <iilleessaannmmii@gmail.com>
2026-04-28 17:10:32 -0500

    i modifird the emoji

--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="Blino's Portfolio", page_icon="👌", layout="wide")
 
-st.title(body="🫥 **Blino's** Portfolio — *Dashboard*", anchor="home", help="yetty has a big head")
+st.title(body="😃 **Blino's** Portfolio — *Dashboard*", anchor="home", help="yetty has a big head")
 st.header("Introduction: Proffessional Summary!")
 st.subheader("Work Experience")
 
